6536463b68e8078a35fb3057249503575d5375ce
Marli Oshlack <marli.oshlack@ephox.com>
2011-09-27 13:51:17 +1000

    Build things

--- src/prelude.js
+++ src/prelude.js
@@ -1,7 +1,1 @@
-var ephox = global.ephox = global.ephox || {};
-var bolt = ephox.bolt = ephox.bolt || {};
 var loader = bolt.loader = bolt.loader || {};
-
-var def = function (deps, factory) {
-    return factory.apply(null, deps);
-};
