4757d184add681beef31b882244dba8f5e011b8b
Marli Oshlack <marli.oshlack@ephox.com>
2011-09-28 11:39:08 +1000

    Clean up xhr.

--- src/transporter/xhr.js
+++ src/transporter/xhr.js
@@ -3,49 +3,56 @@ loader.transporter.xhr = def(
   ],
 
   function () {
-    var request = function() {
-        // FIX is there really a need to look at all these MS impls?
-        var factories = [
-            function () {return new XMLHttpRequest()},
-            function () {return new ActiveXObject("Msxml2.XMLHTTP")},
-            function () {return new ActiveXObject("Msxml3.XMLHTTP")},
-            function () {return new ActiveXObject("Microsoft.XMLHTTP")}
-        ];
-  
-        for (var i = 0; i < factories.length; ++i) {
-            try {
-                return factories[i]();
-            } catch (e) {
-            }
+    var requestObject = function () {
+      // Correct way to use XMLHttpRequest in IE:
+      // http://blogs.msdn.com/b/ie/archive/2006/01/23/516393.aspx
+      var factories = [
+        function () { return new XMLHttpRequest() },
+        function () { return new ActiveXObject("Microsoft.XMLHTTP") }
+      ];
+
+      return fallback(factories);
+    };
+
+    var fallback = function(items) {
+      for (var i = 0; i < items.length; ++i) {
+        try {
+          return items[i]();
+        } catch (e) {
         }
+      }
     };
 
     var handler = function (req, url, success, error) {
-      var done = function() {
-        if (req.status === 200 || req.status === 304)
-          success(req.responseText);
-        else
-          error('Transport error: ' + req.status + ' ' + req.statusText + ' for resource: "' + url + '"');
-      };
-
       return function () {
         if (req.readyState === 4)
-          done();
+          done(req, url, success, error);
       };
     };
 
-    var ajax = function (req, url, success, error) {
+    var done = function(req, url, success, error) {
+      if (req.status === 200 || req.status === 304)
+        success(req.responseText);
+      else
+        error('Transport error: ' + req.status + ' ' + req.statusText + ' for resource: "' + url + '"');
+    };
+
+    var getUrl = function (req, url, success, error) {
       req.open('GET', url, true);
       req.onreadystatechange = handler(req, url, success, error);
       req.send();
     };
 
-    return function(url, success, error) {
-      var req = request();
+    var request = function (url, success, error) {
+      var req = requestObject();
       if (req)
-        ajax(req, url, success, error);
+        getUrl(req, url, success, error);
       else
         error('Transport error: browser does not support XMLHttpRequest.');
+    };
+
+    return {
+      request: request
     };
   }
 );
