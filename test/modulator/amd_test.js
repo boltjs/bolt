bde38c96c6e5419796291e5c2a10bd76e71f6165
Marli Oshlack <marli.oshlack@ephox.com>
2011-09-29 10:41:34 +1000

    Add more tests for amd modulator.

--- test/modulator/amd_test.js
+++ test/modulator/amd_test.js
@@ -1,18 +1,31 @@
-require("../include/include.js");
+require('../include/include.js');
 
 var amd = ephox.bolt.kernel.modulator.amd;
 
 var idTransformer = function (id) {
-  return id.replace(/\./g, '/');
+  return id.replace(/\./g, '/') + '.rocks';
 };
 
-var loader = {
-  load: function (url, onsuccess, onfailure) {
+var loaded = [];
 
+var loader = {
+  load: function () {
+    loaded.push(arguments);
   }
 };
 
-var modulator = amd.create('some.namespace.path', '../../test/path', idTransformer, loader);
+var modulator = amd.create('x.y.z', '../a/b', idTransformer, loader);
 
-assert(modulator.can('some.namespace.path.blah.test'), 'can works for valid id');
-assert(!modulator.can('other.namespace.path.blah.test'), 'can does not work for invalid id');
+assert(modulator.can('x.y.z.blah.test'), 'can works for valid id');
+assert(!modulator.can('z.y.x.blah.test'), 'can does not work for invalid id');
+
+var triple = modulator.modulate('x.y.z.fm.radio');
+
+assert(triple.url === '../a/b/x/y/z/fm/radio.rocks', 'modulator url transformation');
+assert(!triple.serial, 'modulator is not serial');
+
+triple.load('success', 'failure');
+
+assert(loaded.length === 1 && loaded[0][0] === '../a/b/x/y/z/fm/radio.rocks', 'modulator load receives url');
+assert(loaded.length === 1 && loaded[0][1] === 'success', 'modulator passes through success callback');
+assert(loaded.length === 1 && loaded[0][2] === 'failure', 'modulator passes through failure callback');
