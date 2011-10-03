cb2ccca006a4c8c117afb10f659ba0ac4421559b
Marli Oshlack <marli.oshlack@ephox.com>
2011-10-03 14:31:06 +1000

    Add piggybacker and tests.

--- test/fp/object_test.js
+++ test/fp/object_test.js
@@ -10,12 +10,12 @@ obj.each({a: 1, b: 2, c: 3}, function (k, v) {
   values.push(v);
 });
 
-assert(ar.equals(keys, ['a', 'b', 'c']), "oeach passes key as first argument");
-assert(ar.equals(values, [1, 2, 3]), "oeach passes value as second argument");
+assert(ar.equals(keys, ['a', 'b', 'c']), "object each passes key as first argument");
+assert(ar.equals(values, [1, 2, 3]), "object each passes value as second argument");
 
 
 var maps = obj.map({a: 1, b: 2, c: 3}, function (k, v) {
   return v * 2;
 });
 
-assert(maps.a === 2 && maps.b === 4 && maps.c === 6, "omap should map values");
+assert(maps.a === 2 && maps.b === 4 && maps.c === 6, "object map should map values");
