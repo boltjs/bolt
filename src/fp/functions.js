c3acf1a3542c984c3677ca2f040c1485801a2880
Marli Oshlack <marli.oshlack@ephox.com>
2011-10-03 15:01:54 +1000

    Add not and apply functions.

--- src/fp/functions.js
+++ src/fp/functions.js
@@ -12,8 +12,22 @@ kernel.fp.functions = def(
       };
     };
 
+    var not = function (z) {
+      return function () {
+        var slice = Array.prototype.slice;
+        return !z.apply(null, slice.call(arguments, 0));
+      };
+    };
+
+    var apply = function (f) {
+      var slice = Array.prototype.slice;
+      return f.apply(null, slice.call(arguments, 0));
+    };
+
     return {
-      curry: curry
+      curry: curry,
+      not: not,
+      apply: apply
     };
   }
 );
