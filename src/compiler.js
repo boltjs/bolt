6e5fc9314e5dce5cf99b9b4d893968519a252233
markhibberd <mark.hibberd@ephox.com>
2011-10-04 09:15:56 +1000

    doco

--- src/compiler.js
+++ src/compiler.js
@@ -12,23 +12,19 @@ var fn = ephox.bolt.kernel.fp.functions;
 global['define'] = store.define;
 
 /*
- * This is the core compiler process, it recursively
- * identifies and loads module definitions to determine
- * dependencies. Once we know about all modules, we
- * then traverse and print them out, in-order, depth-first.
+ * This is the core compiler process, it  identifies and loads module 
+ * definitions to determine dependencies. Once we know about all modules, 
+ * we then traverse and print them out, in-order, depth-first.
  *
  *  1. analyse current state, to detemine and modules that need to load
  *     - if there was a cycle, fail
- *  2. if there are no modules to load, we are done
- *     - depth-first traversal of dependencies
- *     - output module file (wrapped if necessary)
- *  3. for each id to load
+ *  2. while there are ids to load; for each id to load
  *     - validate that we know how to load
  *     - validate that we have not defined the module already
  *     - convert ids to specifications
  *     - store spec
  *     - if required, evaluate module for to determine dependencies
- *  4. recurse until no more modules to load
+ *  3.  depth-first traversal of dependencies, outputing to standard out.
  */
 var compile = function (modulator, root, modules) {
     var load = fn.curry(loader.load, store, modulator, root);
