f71fee83dd5b910bbfbf3b12fdc8d48f3ff7caa2
Marli Oshlack <marli.oshlack@ephox.com>
2011-10-05 15:51:55 +1000

    Remove POSTSCOPE logic. Add spaces back into test output.

--- components/assert.js
+++ components/assert.js
@@ -1,8 +1,8 @@
 global['assert'] = function (expression, message) {
   if (expression) 
-    console.log("[passed] %s", message || "");
+    console.log("  [passed] %s", message || "");
   else {
-    console.log("[failed] %s", message || "");
+    console.log("  [failed] %s", message || "");
     process.exit(1);
   } 
 };
