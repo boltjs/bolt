a82926624905d70b103f149e03dc0f28cbe849be
markhibberd <mark.hibberd@ephox.com>
2011-10-06 13:51:47 +1000

    crap

---
+++ src/tools/io.js
@@ -0,0 +1,30 @@
+compiler.core.entry = def(
+  [
+    require('fs'),
+    require('path')
+  ],
+
+  function (fs, path) {
+    var read = function (file) {
+        return fs.readFileSync(file, 'UTF-8');
+    };
+
+    var readall = function (files) {
+      return files.map(read);
+    };
+
+    var write = function (file, contents) {
+      return fs.writeFileSync(file, contents);
+    };
+
+    var exists = function (file) {
+      return path.
+    };
+
+    return {
+      read: read,
+      readall: readall,
+      write: write
+    };
+  }
+);
