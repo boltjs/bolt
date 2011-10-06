368b5fe7e79b7eaaec3741a34a55d1b021140ccd
Marli Oshlack <marli.oshlack@ephox.com>
2011-10-06 14:36:19 +1000

    module list

--- src/mode/compile.js
+++ src/mode/compile.js
@@ -6,9 +6,7 @@ compiler.mode.compile = def(
   function (generator) {
     var run = function (config, outdir /*, mains */) {
       var mains = Array.prototype.slice(arguments, 2);
-      // FIX this needs to be a call into module
-      var hookup = "";
-      generator.generate(outdir + '/bootstrap.js', hookup);
+
       // build a 'prod' config
       // write out bootstrap
       //    1. kernel
@@ -16,9 +14,14 @@ compiler.mode.compile = def(
       //    3. module
       //    4. custom config
       //    5. call into module to kick things off.
+
+      // FIX this needs to be a call into module
+      var hookup = "";
+      generator.generate(outdir + '/bootstrap.js', hookup);
+
       // for each main module
       //   concatenate all modules -- potentially in order for serial case.
-
+      mains.for
     };
 
     return {
