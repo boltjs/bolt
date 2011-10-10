4211a58870348188d165234ad7ea2b86cc62061c
markhibberd <mark.hibberd@ephox.com>
2011-10-10 22:47:16 +1000

    Add wedge into locator to avoid browser calls

--- src/bootstrap/locator.js
+++ src/bootstrap/locator.js
@@ -3,9 +3,17 @@ module.bootstrap.locator = def(
   ],
 
   function () {
-    var locate = function () {
+    // FIX kill this locator thing argghhhhhhhhhhhhh
+    var browser = function () {
       var scripts = document.getElementsByTagName("script");
       return scripts[scripts.length - 1].src;
+    };
+
+    var runtime = module.runtime.locate;
+
+    var locate = function () {
+        var f = runtime || browser;
+        return f();
     };
 
     return {
