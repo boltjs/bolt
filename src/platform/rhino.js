d5ee200068e072e915cd1f6c40824fb30048f864
markhibberd <mark.hibberd@ephox.com>
2011-10-11 08:27:28 +1000

    platform files

---
+++ src/platform/rhino.js
@@ -0,0 +1,9 @@
+ephox = this.ephox || {};
+ephox.bolt = ephox.bolt || {};
+ephox.bolt.module = ephox.bolt.module || {};
+ephox.bolt.module.runtime = ephox.bolt.module.runtime || {};
+ephox.bolt.module.runtime.locate = function () {
+  // FIX no idea how to get an accurate value here, does it matter?
+  return "./bootstrap.js";
+};
+
