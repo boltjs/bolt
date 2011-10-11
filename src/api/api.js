7fa0ffd3006f66dd779c62b9e5673fbd41a00ec6
markhibberd <mark.hibberd@ephox.com>
2011-10-11 14:48:40 +1000

    Add module.api delegation layer for define, require, main, etc.

---
+++ src/api/api.js
@@ -0,0 +1,21 @@
+module.api = def(
+  [
+    module.runtime
+  ],
+  
+  function (runtime) {
+    var delegate = function (method) {
+      return function () {
+        runtime[method].apply(null, arguments);
+      };
+    };
+
+    return {
+      configure: delegate('configure'),
+      define: delegate('define'),
+      require: delegate('require'),
+      demand: delegate('demand'),
+      main: delegate('main')
+    };
+  }
+);
