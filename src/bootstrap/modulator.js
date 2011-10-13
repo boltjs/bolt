8c92d1f18af829628da77253c947ffc14529a22a
Marli Oshlack <marli.oshlack@ephox.com>
2011-10-13 14:57:50 +1000

    wire in compiled modulator

--- src/bootstrap/modulator.js
+++ src/bootstrap/modulator.js
@@ -2,21 +2,27 @@ module.bootstrap.modulator = def(
   [
     module.error.error,
     module.modulator.amd,
+    module.modulator.compiled,
     module.modulator.js
   ],
 
-  function (error, amd, js) {
+  function (error, amd, compiled, js) {
+    var builtins = {
+      amd: amd,
+      compiled: compiled,
+      js: js
+    };
+
+    // FIX MTH this needs to support arbitrary module ids as a type, implement delegating modulator to handle them.
     var modulator = function (pather) {
       return function(type /*, args */) {
         var args = Array.prototype.slice.call(arguments, 1);
 
-        if (type === 'amd' || type === 'js') {
-          var factory = type === 'amd' ? amd : js;
-          return factory.create.apply(null, [ pather ].concat(args));
-        }
+        if (builtins[type] === undefined)
+          error.die('Configure error: modulator "' + type + '" is not supported');
 
-        // FIX: allow other types
-        error.die('Configure error: modulator "' + type + '" is not supported');
+        var all = [ pather ].concat(args);
+        builtins[type].create.apply(null, all);
       };
     };
 
