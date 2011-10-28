f2e565f1b96ad2e96565a33b226edf638e2effad
markhibberd <mark.hibberd@ephox.com>
2011-10-28 15:42:55 +1000

    cleanup compound

--- src/modulator/compound.js
+++ src/modulator/compound.js
@@ -3,14 +3,14 @@ kernel.modulator.compound = def(
   ],
 
   function () {
-    var create = function (find) {
+    var create = function (oracle) {
       var can = function (id, demand) {
-        var r = find(id, demand);
+        var r = oracle(id, demand);
         return r.found !== undefined;
       };
 
       var modulate = function (id, define, require, demand) {
-        var r = find(id, demand);
+        var r = oracle(id, demand);
         if (r.notfound)
           throw 'assertion error: can should be used to validate before calls to modulate';
         return r.found.modulate(id, define, require, demand);
