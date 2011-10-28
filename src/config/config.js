8c756e235cc12c5c6708dc0b4a2927c8b0de26a9
markhibberd <mark.hibberd@ephox.com>
2011-10-28 15:43:18 +1000

    working, all sorted out

--- src/config/config.js
+++ src/config/config.js
@@ -2,18 +2,20 @@ module.config.config = def(
   [
     module.error.error,
     module.config.modulator,
+    module.config.source,
     module.config.apiwrapper,
     ephox.bolt.kernel.api.config,
     ephox.bolt.kernel.modulator.compound
   ],
 
-  function (error, modulator, apiwrapper, config, compound) {
+  function (error, modulator, source, apiwrapper, config, compound) {
     var configure = function (configuration, pather) {
       var sourcespecs = configuration.sources || [];
       var modulatorspecs = configuration.modulators || [];
       var modulatorsources = modulator.sources(modulatorspecs, pather);
       var modulatortypes = modulator.types(modulatorspecs);
-      var combined = compound.create(modulatorsources, modulatortypes, sourcespecs, pather);
+      var oracle = source.build(modulatorsources, modulatortypes, sourcespecs, pather);
+      var combined = compound.create(oracle);
       var bolt = config.configure(combined, error.die);
       return apiwrapper.api(bolt, modulatorspecs, modulatortypes);
     };
---
+++ src/config/source.js
@@ -0,0 +1,45 @@
+module.config.source = def(
+  [
+    ephox.bolt.kernel.fp.array
+  ],
+
+  function (ar) {
+    var find = function (id, sources) {
+      for (var i = 0; i < sources.length; ++i)
+        if (sources[i].can(id))
+          return { found: sources[i] };
+      return { notfound: true };
+    };
+
+    var get = function (spec, demand, modulatortypes) {
+      var type = spec.type;
+      var id = modulatortypes[type];
+      if (id === undefined)
+        throw 'Configuration error: no modulator for source: ' + type;
+      return demand(id);
+    };
+
+    var initialise = function (demand, modulatorsources, modulatortypes, sourcespecs, pather) {
+      var sources =  ar.map(sourcespecs, function (spec) {
+        var modulator = get(spec, demand, modulatortypes);
+        var args = [ pather ].concat(spec.args);
+        return modulator.create.apply(null, args);
+      });
+      return modulatorsources.concat(sources);
+    };
+
+    var build = function (modulatorsources, modulatortypes, sourcespecs, pather)  {
+      return function (id, demand) {
+        var r = find(id, modulatorsources);
+        if (r.found !== undefined)
+          return r;
+        var sources = initialise(demand, modulatorsources, modulatortypes, sourcespecs, pather);
+        return find(id, sources);
+      };
+    };
+    
+    return {
+      build: build
+    };
+  }
+);
