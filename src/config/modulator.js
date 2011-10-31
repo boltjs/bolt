045a683430de5396a69e46665890f0f1d38e69b7
markhibberd <mark.hibberd@ephox.com>
2011-10-31 11:32:59 +1000

    Refactor modulator. Add missing dependency.

--- src/config/modulator.js
+++ src/config/modulator.js
@@ -1,10 +1,11 @@
 module.config.modulator = def(
   [
     module.modulator.globalator,
+    module.modulator.amd,
     ephox.bolt.kernel.fp.array
   ],
 
-  function (globalator, ar) {
+  function (globalator, amd, ar) {
     var types = function (specs) {
       var ts = {
         amd: 'global!ephox.bolt.module.modulator.amd',
@@ -17,15 +18,20 @@ module.config.modulator = def(
       return ts;
     };
 
+    var source = function (spec, pather) {
+      return amd.create(pather, spec.namespace, spec.path, spec.mapper);
+    };
+
     var sources = function (specs, pather) {
       return ar.map(specs, function (spec) {
-        return amd.create(pather, spec.namespace, spec.path, spec.mapper);
+        return source(spec, pather);
       }).concat([ globalator.create() ]);
     };
 
     return {
       types: types,
-      sources: sources
+      sources: sources,
+      source: source
     };
   }
 );
