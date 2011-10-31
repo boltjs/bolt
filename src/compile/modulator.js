091265b7c9abe1bfa8a658f1b16dfbb2df49f85e
markhibberd <mark.hibberd@ephox.com>
2011-10-31 11:56:03 +1000

    Added new config support for compiler

--- src/compile/modulator.js
+++ src/compile/modulator.js
@@ -2,46 +2,44 @@ compiler.compile.modulator = def(
   [
     ephox.bolt.kernel.fp.array,
     ephox.bolt.kernel.api.config,
-    ephox.bolt.kernel.modulator.amd,
     ephox.bolt.kernel.modulator.compound,
+    ephox.bolt.module.modulator.globalator,
     ephox.bolt.module.config.modulator,
-    ephox.bolt.module.config.source,
+    compiler.config.source,
     compiler.loader.node,
-    compiler.tools.error
+    compiler.tools.error,
+    compiler.modulator.amd,
+    compiler.modulator.compiled,
+    compiler.modulator.js
   ],
 
-  function (ar, config, amd, compound, modulator, source, node, error) {
+  function (ar, config, compound, globalator, modulator, source, node, error, amd, compiled, js) {
+    var instantiate = function (source, id) {
+      var bolt = config.configure(source, error.die);
+      global.define = bolt.define;
+      bolt.require([id], function () {});
+      return bolt.demand(id);
+    };
+
+    var instances = function (specs, pather) {
+      var is = {
+        amd: amd,
+        js: js,
+        compiled: compiled
+      };
+      ar.each(specs, function (spec) {
+        var source = modulator.source(spec, pather);
+        is[spec.type] = instantiate(source, spec.compiler);
+      });
+      return is;
+    };
 
     var load = function (configuration, pather) {
-      var maker = function (spec) {
-        // FIX pull out
-        var source = amd.create(pather, spec.impl, spec.path, spec.mapper, node);
-        var bolt = config.configure(source, error.die);
-        var id = spec.impl + '.Compiler';
-        global.define = bolt.define;
-        var instance;
-        // FIX we know this is synchronous because of node loader, we probably need a demand that will let us load things synchronously
-        bolt.require([id], function (modulator) {
-          instance = modulator;
-        });
-
-        return function (/* supplied */) {
-          return instance.create.apply(null, arguments);
-        };
-      };
-
-      var builtins = {
-        amd: compiler.modulator.amd.create,
-        compiled: compiler.modulator.compiled.create,
-        js: compiler.modulator.js.create
-      };
-
-      // FIX dedupe
       var modulatorspecs = configuration.modulators || [];
       var sourcespecs = configuration.sources || [];
-      var modulators = modulator.modulators(modulatorspecs, builtins, maker);
-      var sources = source.sources(sourcespecs, modulators, pather);
-      return compound.create(modulators, sources);
+      var modulators = instances(modulatorspecs, pather);
+      var oracle = source.build(modulators, sourcespecs, pather);
+      return compound.create(oracle);
     };
 
     return {
--- src/compile/source.js
+++ src/compile/source.js
@@ -1,15 +1,31 @@
-compile.source = def(
+compiler.config.source = def(
   [
+    ephox.bolt.kernel.fp.array
   ],
 
-  function () {
-    var create = function (configuration) {
-
-
+  function (ar) {
+    var buildsource = function (modulators, sourcespecs, pather) {
+      return ar.map(sourcespecs, function (spec) {
+        var modulator = modulators[spec.type];
+        if (modulator === undefined)
+          throw "could not find modulator for type: " + spec.type;
+        var args = [ pather ].concat(spec.args);
+        return modulator.create.apply(null, args);
+      });
     };
 
+    var build = function (modulators, sourcespecs, pather)  {
+      var sources = buildsource(modulators, sourcespecs, pather);
+      return function (id) {
+        for (var i = 0; i < sources.length; ++i)
+          if (sources[i].can(id))
+            return { found: sources[i] };
+        return { notfound: true };
+      };
+    };
+    
     return {
-      create: create
+      build: build
     };
   }
 );
