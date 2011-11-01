66b88f1dba162fd016abc68960e1b31e9a1e990a
markhibberd <mark.hibberd@ephox.com>
2011-11-01 10:51:14 +1000

    Remove debug code accidentally checked in.

--- src/tools/error.js
+++ src/tools/error.js
@@ -5,11 +5,8 @@ compiler.tools.error = def(
   function () {
     var die = function (message, code) {
       console.error("error: " + message + "\n");
-//        process.exit(code || 1);
-      brick;
+      process.exit(code || 1);
     };
-
-
 
     return {
       die: die
