76d4d6c88494c62263d856d86136c9162a72093f
markhibberd <mark.hibberd@ephox.com>
2011-11-04 15:41:07 +1000

    Whitespace fix.

--- components/demo.js
+++ components/demo.js
@@ -7,15 +7,15 @@ var statusMaker = function (type, colour) {
 var pass = statusMaker('passed', 'green');
 var fail = statusMaker('failed', 'red');
 
-var runDemo = function(name, fn) {
-  $(document).ready(function() {
+var runDemo = function (name, fn) {
+  $(document).ready(function () {
     var heading = $('<h3>' + name + '</h3>');
     $('body').append(heading);
     fn();
   });
 };
 
-var createOutputElement = function(message) {
+var createOutputElement = function (message) {
   var div = $('<div />');
   $('body').append(div);
   fail(div, message);
