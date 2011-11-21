a517f8e58dcd0749e727254e06ffc00bc7f4fe91
markhibberd <mark.hibberd@ephox.com>
2011-11-21 11:36:52 +1000

    tests

--- test/modulator/amd_test.js
+++ test/modulator/amd_test.js
@@ -23,11 +23,11 @@ assert(!modulator.can('z.y.x.blah.test'), 'can does not work for invalid id');
 
 var triple = modulator.get('x.y.z.fm.radio');
 
-assert(triple.url === '../a/b/x/y/z/fm/radio.rocks', 'modulator url transformation');
+assert(triple.url === '../a/b/x/y/z/fm/radio.rocks.js', 'modulator url transformation');
 assert(!triple.serial, 'modulator is not serial');
 
 triple.load('success', 'failure');
-
-assert(loaded.length === 1 && loaded[0][0] === '../a/b/x/y/z/fm/radio.rocks', 'modulator load receives url');
+ 
+assert(loaded.length === 1 && loaded[0][0] === '../a/b/x/y/z/fm/radio.rocks.js', 'modulator load receives url');
 assert(loaded.length === 1 && loaded[0][1] === 'success', 'modulator passes through success callback');
 assert(loaded.length === 1 && loaded[0][2] === 'failure', 'modulator passes through failure callback');
