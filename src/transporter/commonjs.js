ad4d0cf36ead9de532bdfaf753c9997938c77934
markhibberd <mark.hibberd@ephox.com>
2011-11-22 17:51:32 +1000

    Move extention mapping to modulator.

---
+++ src/transporter/commonjs.js
@@ -0,0 +1,26 @@
+loader.transporter.commonjs = def(
+  [
+  ],
+
+  function () {
+    var read = function (url, success, error) {
+      var fs = require('fs');
+      var path = require('path');
+      path.exists(url, function (exists) {
+        if (exists)
+          fs.readFile(url, 'UTF-8', function (err, data) {
+            if (err)
+              error('Error reading file [' + url + '], error [' + err + ']');
+            else
+              success(data);
+          });
+        else
+          error('File does not exist [' + url + ']');
+      });
+    };
+
+    return {
+      read: read
+    };
+  }
+);
