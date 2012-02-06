c36fa80ec1d531aee3a4e7403fd6efe17e5ed8e7
markhibberd <mark.hibberd@ephox.com>
2012-02-06 13:55:33 +1000

    Added arguments to main.

--- src/bootstrap/install.js
+++ src/bootstrap/install.js
@@ -1,15 +1,14 @@
 module.bootstrap.install = def(
   [
-     module.config.builtins,
+    module.config.builtins,
     module.config.config,
     module.config.specs,
     module.mapper.mapper,
     module.bootstrap.deferred,
-    module.runtime,
-    ephox.bolt.kernel.fp.functions
+    module.runtime
   ],
 
-  function (builtins, config, specs, mapper, deferred, runtime, fn) {
+  function (builtins, config, specs, mapper, deferred, runtime) {
     var install = function (pather) {
       var configure = function (configuration) {
         var bolt = config.configure(configuration, pather, builtins.browser);
@@ -21,17 +20,21 @@ module.bootstrap.install = def(
         deferred.configured(runtime.require);
       };
 
+      var main = function (id, args, configs, callback) {
+        runtime.require(configs || [], function () {
+          callback && callback.apply(null, arguments);
+          runtime.require([id], function (module) {
+            module.apply(null, args);
+          });
+        });
+      };
+
       runtime.configure = configure;
       runtime.source = specs.source;
       runtime.type = specs.type;
       runtime.modulator = specs.type; // FIX transitioning to type, kill after Wed 7th December.
       runtime.require = deferred.require;
-      runtime.main = function (id, configs, callback) {
-        runtime.require(configs || [], function () {
-          callback && callback.apply(null, arguments);
-          runtime.require([id], fn.apply);
-        });
-      };
+      runtime.main = main;
     };
 
     return {
