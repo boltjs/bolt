54eac9d7e1855af815125b9da6d9541c13b6c182
markhibberd <mark.hibberd@ephox.com>
2012-02-15 16:53:36 +1000

    Testing cleanup.

--- src/bolt-test.js
+++ src/bolt-test.js
@@ -1,5 +1,7 @@
-var configfile = process.argv[2];
-var tests = process.argv.slice(3); // argv[0] = node, argv[1] = jsc.js, argv[2] = config
+// argv[0] = node, argv[1] = jsc.js, argv[2] = verbose, argv[3] = config, argv[4..n] = tests
+var verbose = process.argv[2];
+var configfile = process.argv[3];
+var tests = process.argv.slice(4);
 
 require('./kernel');
 require('./loader');
@@ -9,7 +11,7 @@ require('./jssert');
 
 var path = require('path');
 var runner = ephox.bolt.test.run.runner;
-var reporter = ephox.bolt.test.report.vanilla;
+var reporter = ephox.bolt.test.report.logger.create(verbose === 'true');
 var fn = ephox.bolt.kernel.fp.functions;
 var node = ephox.bolt.module.reader.node;
 var reader = fn.curry(node.read, process.cwd() + '/.', configfile);
