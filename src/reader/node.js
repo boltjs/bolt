e3a79785062dd475b3a34261673f40b038249949
markhibberd <mark.hibberd@ephox.com>
2012-02-20 09:42:56 +1000

    resolve to absolute for bouncing

--- src/reader/node.js
+++ src/reader/node.js
@@ -9,7 +9,7 @@ module.reader.node = def(
       var path = require('path');
       var accumulated = acc || { sources: [], types: [],  configs: [] };
       var base = path.dirname(relativeto);
-      var absolute = path.relative(base, file);
+      var absolute = path.resolve(base, file);
       var payload = fs.readFileSync(absolute, 'UTF-8');
       bouncing.evaluate(absolute, payload, done, read, accumulated);
     };
