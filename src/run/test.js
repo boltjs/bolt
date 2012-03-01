28451305f98f60bd10a53849de91eede26e181b9
markhibberd <mark.hibberd@ephox.com>
2012-03-01 15:03:52 +1000

    Added transparent script loader api

--- src/run/test.js
+++ src/run/test.js
@@ -4,17 +4,18 @@ test.run.test = def(
     ephox.bolt.module.config.builtins.commonjs,
     ephox.bolt.module.bootstrap.install,
     ephox.bolt.loader.transporter.commonjs.read,
+    ephox.bolt.loader.api.commonjsevaller.load,
     test.run.wrapper
   ],
 
-  function (fn, builtins, install, transport, wrapper) {
+  function (fn, builtins, install, load, loadscript, wrapper) {
     return function (next, reporter, reader, testfile, name, deps, fn) {
       if (typeof deps === 'function' && fn === undefined) {
         fn = deps;
         deps = [];
       }
 
-      install.install(reader, builtins, transport);
+      install.install(reader, builtins, load, loadscript);
 
       var wrapped = wrapper.wrap(reporter, testfile, name, fn, next);
 
