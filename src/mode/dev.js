735515950c79837d874016aff82c5684616e817e
markhibberd <mark.hibberd@ephox.com>
2012-03-21 21:27:40 +1000

    dev mode changes

--- src/mode/dev.js
+++ src/mode/dev.js
@@ -5,14 +5,16 @@ compiler.mode.dev = def(
 
   function (generator) {
     var run = function (bootstrap, config) {
-      config = config || 'module.js';
+      var path = require('path');
+      var relative = path.relative(path.dirname(bootstrap), config);
+
       var hookup =
         '(function () {\n' +
         '  var obj = ephox.bolt.kernel.fp.object;\n' +
         '  var api = ephox.bolt.module.api;\n' +
         '  var builtins = ephox.bolt.module.config.builtins.browser;\n' +
         '  var install = ephox.bolt.module.bootstrap.install;\n' +
-        '  var reader = ephox.bolt.module.bootstrap.configloader.create("' + config + '");\n' +
+        '  var reader = ephox.bolt.module.bootstrap.configloader.create("' + relative + '");\n' +
         '  var transport = ephox.bolt.loader.transporter.xhr.request;\n' +
         '  var script = ephox.bolt.loader.api.scripttag.load;\n' +
         '  install.install(reader, builtins, transport, script);\n' +
