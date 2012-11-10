187e4eac320c63832751b97de8c99bae5445e89c
Marli Oshlack <marli@oshlack.com>
2012-11-11 09:17:06 +1000

    Clean up jsc.bat.

--- script/jsc.bat
+++ script/jsc.bat
@@ -1,24 +1,9 @@
 @echo off
 setlocal enableextensions
 
-goto entry
-
-:fail
-  exit /b 1
-
-:entry
-
 set base=%~dp0
 
 for %%i in (node.bat node.exe) do if not "%%~$PATH:i"=="" set node=%%~$PATH:i
-if "%node%"=="" echo error: node.js is not on the system path && goto fail
+if "%node%"=="" echo error: node.js is not on the system path && exit /b 1
 
-set remaining=
-:jsc_remaining
-if "%~1"=="" goto end_jsc_remaining
-set remaining=%remaining% "%~1"
-shift
-goto jsc_remaining
-:end_jsc_remaining
-
-"%node%" "%base%jsc.js" %remaining%
+"%node%" "%base%jsc.js" %*
