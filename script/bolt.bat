565b77d4947fe528441d7265f83f13299d5ab51f
Marli Oshlack <marli@oshlack.com>
2012-11-11 19:11:24 +1000

    Port bolt command line parsing logic into JS.

--- script/bolt.bat
+++ script/bolt.bat
@@ -1,82 +1,9 @@
 @echo off
-setlocal enableextensions enabledelayedexpansion
-
-goto entry
-
-:usage
-  echo usage: bolt [-h^|--help] [-V^|--version]
-  echo     or bolt MODE [^<args^>]
-  echo.
-  echo modes:
-  echo   init
-  echo   build
-  echo   test
-  echo   help
-  echo.
-  echo For usage/help on a specific mode, use:
-  echo   bolt help MODE
-
-  exit /b 0
-
-:fail_usage
-  echo.
-  call :usage
-  exit /b 1
-
-:fail
-  exit /b 1
-
-:version
-  for /F "usebackq delims=" %%i in ("%base%version") do set v=%%i
-  echo bolt %v%
-  exit /b 0
-
-
-:entry
+setlocal enableextensions
 
 set base=%~dp0
 
-if "%~1"=="" echo error: must specify mode. && goto fail_usage
+for %%i in (node.bat node.exe) do if not "%%~$PATH:i"=="" set node=%%~$PATH:i
+if "%node%"=="" echo error: node.js is not on the system path && exit /b 1
 
-set mode=%~1
-shift
-
-if "%mode%"=="init" goto done_mode_validation
-if "%mode%"=="build" goto done_mode_validation
-if "%mode%"=="test" goto done_mode_validation
-if "%mode%"=="help" goto done_mode_validation
-if "%mode%"=="-h" call :usage && exit /b 0
-if "%mode%"=="--help" call :usage && exit /b 0
-if "%mode%"=="-V" call :version && exit /b 0
-if "%mode%"=="--version" call :version && exit /b 0
-
-echo invalid mode [%mode%], must be one of init^|build^|test^|help
-goto fail_usage
-
-:done_mode_validation
-
-if not "%mode%"=="help" goto run_mode
-
-set help_mode=true
-set mode=%~1
-shift
-
-if "%mode%"=="init" goto run_mode
-if "%mode%"=="build" goto run_mode
-if "%mode%"=="test" goto run_mode
-
-echo help requires argument: bolt help MODE, where MODE is one of init^|build^|test
-exit /b 2
-
-:run_mode
-
-set remaining=
-:inline_remaining
-if "%~1"=="" goto end_inline_remaining
-set remaining=%remaining% "%~1"
-shift
-goto inline_remaining
-:end_inline_remaining
-
-call "%base%bolt-%mode%.subr.bat" %remaining%
-exit /b %errorlevel%
+"%node%" "%base%bolt.js" %*
---
+++ script/bolt.js
@@ -0,0 +1,83 @@
+var usage = function () {
+  return 'usage: bolt [-h|--help] [-V|--version]\n' +
+         '    or bolt MODE [<args>]\n' +
+         '\n' +
+         'modes:\n' +
+         '  init\n' +
+         '  build\n' +
+         '  test\n' +
+         '  help\n' +
+         '\n' +
+         'For usage/help on a specific mode, use:\n' +
+         '  bolt help MODE\n';
+};
+
+var fail_usage = function (code, message) {
+  console.error(message);
+  console.error('');
+  console.error(usage());
+  process.exit(code);
+};
+
+var fail = function (code, message) {
+  console.error(message);
+  process.exit(code);
+};
+
+var version = function () {
+  var v = fs.readFileSync(__dirname + '/version', 'UTF-8');
+  if (v[v.length - 1] === '\n')
+    v = v.substring(0, v.length - 1);
+  return v;
+};
+
+
+process.argv.shift();  // argv[0] = node
+process.argv.shift();  // argv[1] = bolt.js
+
+if (process.argv.length < 1)
+  fail_usage(1, 'error: must specify mode.');
+
+var mode = process.argv[0];
+process.argv.shift();
+
+var fs = require('fs');
+
+switch (mode) {
+  case '-h':
+  case '--help':
+    console.log(usage());
+    process.exit();
+  case '-V':
+  case '--version':
+    console.log('bolt ' + version());
+    process.exit();
+  case 'init':
+  case 'build':
+  case 'test':
+  case 'help':
+    break;
+  default:
+    fail_usage(1, 'invalid mode [' + mode + '], must be one of init|build|test|help');
+}
+
+
+var help_mode = false;
+
+if (mode === 'help') {
+  help_mode = true;
+  mode = process.argv[0];
+  switch (mode) {
+    case 'init':
+    case 'build':
+    case 'test':
+      break;
+    default:
+      fail(2, 'help requires argument: bolt help MODE, where MODE is one of init|build|test');
+  }
+}
+
+console.log('help_mode: ', help_mode);
+console.log('mode: ', mode);
+
+// . $base/bolt-$mode.subr
