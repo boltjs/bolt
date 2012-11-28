ab4e887c724101451d95352b31761d600ea50cf4
Marli Oshlack <marli.oshlack@ephox.com>
2012-11-28 12:20:06 +1000

    Clean up bolt build usage.

--- script/bolt-build.js
+++ script/bolt-build.js
@@ -1,29 +1,33 @@
 var usage = function () {
-  return 'usage: bolt build [-c|--config CONFIG_JS] [-o|--output OUTPUT_DIR] [-s|--src SRC_DIR]\n' +
-         '                  [-i|--inline] [-n|--invoke-main MAIN_MODULE] [-r|--register]\n' +
-         '                  [-m|--modules] [-e|--entry-points FILE ...] [-g|--entry-group NAME FILE ...]\n' +
+  return 'usage: bolt build [-c|--config CONFIG_JS] [-o|--output OUTPUT_DIR]\n' +
+         '                  [-s|--src SRC_DIR] [-i|--inline]\n' +
+         '                  [-n|--invoke-main MAIN_MODULE] [-r|--register] [-m|--modules]\n' +
+         '                  [-e|--entry-points FILE ...] [-g|--entry-group NAME FILE ...]\n' +
          '\n' +
          'options:\n' +
-         '  -c|--config CONFIG_JS          override bolt configuration file\n' +
+         '  -c|--config CONFIG_JS          override bolt configuration file.\n' +
          '                                   default: config/bolt/prod.js\n' +
-         '  -o|--output OUTPUT_DIR         override output directory, compiled output will be\n' +
-         '                                 located at $OUTPUT_DIR/compile, inline output at\n' +
-         '                                 $OUTPUT_DIR/inline, modules at $OUTPUT_DIR/module\n' +
+         '  -o|--output OUTPUT_DIR         override build output directory. Compiled\n' +
+         '                                 output will be located at $OUTPUT_DIR/compile,\n' +
+         '                                 inline output at $OUTPUT_DIR/inline, modules at\n' +
+         '                                 $OUTPUT_DIR/module.\n' +
          '                                   default: scratch/main/js\n' +
-         '  -s|--src SRC_DIR               override source directory\n' +
+         '  -s|--src SRC_DIR               override source directory.\n' +
          '                                   default: src/main/js\n' +
-         '  -i|--inline                    enable generation of inline scripts (only produces output\n' +
-         '                                 in conjunction with -e or -g).\n' +
-         '  -n|--invoke-main MAIN_MODULE   specify main module of inline scripts.\n' +
-         '  -r|--register                  register modules in global namespace for inline scripts,\n' +
-         '                                 this will default to true unless -n is specified.\n' +
+         '  -i|--inline                    enable generation of inline scripts (only\n' +
+         '                                 produces output in conjunction with -e or -g).\n' +
+         '  -n|--invoke-main MAIN_MODULE   specify a main module for inline scripts.\n' +
+         '  -r|--register                  register modules in a global namespace for\n' +
+         '                                 inline scripts. Defaults to true unless -n is\n' +
+         '                                 specified.\n' +
          '  -m|--modules                   enable generation of flat module files.\n' +
-         '  -e|--entry-points FILE ...     specify a set of entry points, a compiled output\n' +
-         '                                 will be generated for each entry point. Multiple\n' +
-         '                                 -e flags may be specified.\n' +
-         '  -g|--entry-group NAME FILE ... specify an entry group, a single compiled output\n' +
-         '                                 will be generated with NAME for each entry-group.\n' +
-         '                                 Multiple -g flags may be specified.\n' +
+         '  -e|--entry-points FILE ...     specify a set of entry points. A compiled\n' +
+         '                                 output will be will be generated for each entry\n' +
+         '                                 point. Multiple -e flags may be specified.\n' +
+         '  -g|--entry-group NAME FILE ... specify an entry group. A single compiled\n' +
+         '                                 output will be generated with NAME for each\n' +
+         '                                 entry group. Multiple -g flags may be\n' +
+         '                                 specified.\n' +
          '\n' +
          'example:\n' +
          '  Produce a bolt build for a top level application. A compiled file will be\n' +
@@ -32,26 +36,26 @@ var usage = function () {
          '    bolt build -e src/main/js/**/*Main.js\n' +
          '\n' +
          '\n' +
-         '  Produce a bolt build for a top level library. A self contained script registering\n' +
-         '  all modules in their namespace will be produced.\n' +
+         '  Produce a bolt build for a top level library. A self-contained script\n' +
+         '  registering all modules globally in their namespace will be produced.\n' +
          '\n' +
          '    bolt build -i -g example src/main/js/**/api/*.js\n' +
          '\n' +
          '\n' +
-         '  Produce a bolt build for a general purpose library, in this build we only want\n' +
+         '  Produce a bolt build for a general purpose library. In this build we only want\n' +
          '  modules to be produced, no compiled output.\n' +
          '\n' +
          '    bolt build -m src/main/js\n' +
          '\n' +
          '\n' +
          'note:\n' +
-         '  Examples assume use of a shell with "**" glob support. This means either zsh or\n' +
-         '  bash 4.x with `shopt -s globstar` set. If you are an insolent mac user with a\n' +
-         '  default bash 3.x, this tool strongly recommends you upgrade (although defenestration\n' +
-         '  of said mac is also a valid option).\n' +
+         '  Examples assume use of a shell with "**" glob support. This means either zsh\n' +
+         '  or bash 4.x with `shopt -s globstar` set. If you are an insolent mac user with\n' +
+         '  a default bash 3.x, this tool strongly recommends you upgrade (although\n' +
+         '  defenestration of said mac is also a valid option).\n' +
          '\n' +
-         '  If you become desperate something like $(find src/test/js/atomic -name \*.js) could be\n' +
-         '  used as a substitute.\n';
+         '  If you become desperate something like $(find src/test/js/atomic -name \*.js)\n' +
+         '  could be used as a substitute.\n';
 }
 
 var fail_usage = function (code, message) {
