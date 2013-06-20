1d6fafba0bf46a5721ac0d958c76e6c4767a1067
markhibberd <mark@hibberd.id.au>
2013-06-20 21:33:01 +1000

    Added global remove and some tests.

--- kernel/src/util/globals.js
+++ kernel/src/util/globals.js
@@ -5,22 +5,28 @@ kernel.util.globals = def(
   function () {
     var global = Function('return this')();
 
-    var scoped = function (name, scope) {
-      var parts = name.split('.');
+    var get = function (parts, scope) {
       var r = scope;
       for (var i = 0; i < parts.length && r !== undefined; ++i)
         r = r[parts[i]];
       return r;
     };
 
-    var resolve = function (name) {
-      return scoped(name, global);
+    var resolve = function (name, scope) {
+      var parts = name.split('.');
+      return get(parts, scope || global);
+    };
+
+    var remove = function (name, scope) {
+      var parts = name.split('.');
+      var parent = get(parts.slice(0, -1), scope || global);
+      delete parent[parts[parts.length - 1]];
     };
 
     return {
       global: global,
-      scoped: scoped,
-      resolve: resolve
+      resolve: resolve,
+      remove: remove
     };
   }
 );
