0d6061b5d2a7c20d9d9258f2a49e2b00937d4347
Marli Oshlack <marli@oshlack.com>
2013-06-29 15:08:44 -0700

    Implement project.json support for bolt test.

--- projects/script/command/build.js
+++ projects/script/command/build.js
@@ -1,11 +1,11 @@
 var usage = function () {
-  return 'usage: bolt build [-p|--project PROJECT_JSON ...] [-c|--config CONFIG_JS]\n' +
+  return 'usage: bolt build [-p|--project PROJECT_FILE] [-c|--config CONFIG_JS]\n' +
          '                  [-s|--src SRC_DIR] [-o|--output OUTPUT_DIR] [-m|--modules]\n' +
          '                  [-n|--inline-main MAIN_MODULE] [-r|--inline-register]\n' +
          '                  [-e|--entry-points FILE ...] [-g|--entry-group NAME FILE ...]\n' +
          '\n' +
          'options:\n' +
-         '  -p|--project PROJECT_JSON      override project configuration file. This json\n' +
+         '  -p|--project PROJECT_FILE      override project configuration file. This json\n' +
          '                                 configuration file format allows defaults to be\n' +
          '                                 specified for all bolt command line arguments.\n' +
          '                                   default: project.json\n' +
@@ -80,7 +80,7 @@ module.exports = function (help_mode) {
     process.exit();
   }
 
-  var project_json = null;
+  var project_file = null;
   var config_js = null;
   var src_dir = null;
   var output_dir = null;
@@ -102,7 +102,7 @@ module.exports = function (help_mode) {
       case '--project':
         if (process.argv.length < 1)
           fail_usage(1, flag + ' requires an argument to be specified');
-        project_json = process.argv[0];
+        project_file = process.argv[0];
         process.argv.shift();
         break;
       case '-c':
@@ -206,13 +206,13 @@ module.exports = function (help_mode) {
   require('./../lib/loader');
   require('./../lib/module');
   require('./../lib/compiler');
-  var project_json_reader = require('./../lib/project-json-reader');
+  var project_file_reader = require('./../lib/project-file-reader');
   var Globals = bolt.kernel.util.Globals;
 
-  if (project_json && (!fs.existsSync(project_json) || !fs.statSync(project_json).isFile()))
-    fail(1, project_json + ' does not exist or is not a file');
+  if (project_file && (!fs.existsSync(project_file) || !fs.statSync(project_file).isFile()))
+    fail(1, project_file + ' does not exist or is not a file');
 
-  var config = project_json_reader.read(project_json || 'project.json');
+  var config = project_file_reader.read(project_file || 'project.json', fail);
 
   config_js = config_js || Globals.resolve('build.config', config) || 'config/bolt/prod.js';
   src_dir = src_dir || Globals.resolve('src', config) || 'src/js';
--- projects/script/command/test.js
+++ projects/script/command/test.js
@@ -1,31 +1,37 @@
 var usage = function () {
-  return 'usage: bolt test [-v|--verbose] CONFIG TEST ...\n' +
+  return 'usage: bolt test [-p|--project PROJECT_FILE] [-v|--verbose] CONFIG TEST ...\n' +
          '\n' +
          'arguments:\n' +
-         '  CONFIG                   The bolt configuration file to be used for tests\n' +
-         '  TEST                     A test file. The test file may contain one or more\n' +
-         '                           test methods. Test files have an `assert` library\n' +
-         '                           exposed to them automatically.\n' +
+         '  CONFIG                      the bolt configuration file to be used for tests.\n' +
+         '  TEST                        a test file. The test file may contain one or more\n' +
+         '                              test methods. Test files have an `assert` library\n' +
+         '                              exposed to them automatically.\n' +
+         '\n' +
+         'options:\n' +
+         '  -p|--project PROJECT_FILE   override project configuration file. This json\n' +
+         '                              configuration file format allows defaults to be\n' +
+         '                              specified for all bolt command line arguments.\n' +
+         '                                default: project.json\n' +
+         '  -v|--verbose                turn on verbose test output.\n' +
          '\n' +
          'example:\n' +
-         '  Run all atomic tests.\n' +
+         '  Run all node tests.\n' +
          '\n' +
-         '    bolt test config/bolt/atomic.js src/test/js/atomic/**/*.js\n' +
+         '    bolt test config/bolt/node-test.js test/js/node/**/*.js\n' +
          '\n' +
          '\n' +
          '  Run all browser tests.\n' +
          '\n' +
-         '    bolt test config/bolt/browser.js src/test/js/browser/**/*.js\n' +
+         '    bolt test config/bolt/browser-test.js test/js/browser/**/*.js\n' +
          '\n' +
          '\n' +
          'note:\n' +
          '  Examples assume use of a shell with "**" glob support. This means either zsh\n' +
          '  or bash 4.x with `shopt -s globstar` set. If you are an insolent mac user with\n' +
-         '  a default bash 3.x, this tool strongly recommends you upgrade (although\n' +
-         '  defenestration of said mac is also a valid option).\n' +
+         '  a default bash 3.x, this tool strongly recommends you upgrade.\n' +
          '\n' +
-         '  If you become desperate something like $(find src/test/js/atomic -name \*.js)\n' +
-         '  could be used as a substitute.\n';
+         '  If you become desperate something like $(find test/js/node -name \*.js) could\n' +
+         '  be used as a substitute.\n';
 };
 
 var fail_usage = function (code, message) {
@@ -47,32 +53,57 @@ module.exports = function (help_mode) {
     process.exit();
   }
 
-  var verbose = false;
+  var project_file = null;
+  var verbose = null;
 
-  switch (process.argv[0]) {
-    case '-v':
-    case '--verbose':
-      verbose = true;
-      process.argv.shift();
-      break;
-    default:
-      if (process.argv[0] && process.argv[0][0] === '-')
-        fail_usage(1, 'Unknown flag [' + process.argv[0] +']');
+  while (process.argv.length > 0 && process.argv[0][0] === '-') {
+    var flag = process.argv[0];
+    process.argv.shift();
+
+    switch (flag) {
+      case '-p':
+      case '--project':
+        if (process.argv.length < 1)
+          fail_usage(1, flag + ' requires an argument to be specified');
+        project_file = process.argv[0];
+        process.argv.shift();
+        break;
+      case '-v':
+      case '--verbose':
+        verbose = true;
+        break;
+      case '--':
+        break;
+      default:
+        fail_usage(1, 'invalid flag [' + flag +']');
+    }
   }
-
-
-  if (process.argv.length < 2)
-    fail_usage(1, 'Not enough arguments, must specify configuration and at least one test file.');
-
-  var config = process.argv[0];
-  process.argv.shift();
 
   var fs = require('fs');
 
-  if (!fs.existsSync(config) || !fs.statSync(config).isFile())
-    fail(10, 'Could not find config file [' + config + ']');
+  require('./../lib/kernel');
+  require('./../lib/loader');
+  require('./../lib/module');
+  require('./../lib/test');
+  var project_file_reader = require('./../lib/project-file-reader');
+  var Globals = bolt.kernel.util.Globals;
 
-  var tests = process.argv.slice(0);
+  if (project_file && (!fs.existsSync(project_file) || !fs.statSync(project_file).isFile()))
+    fail(1, project_file + ' does not exist or is not a file');
+
+  var config = project_file_reader.read(project_file || 'project.json', fail);
+
+  verbose = verbose || Globals.resolve('test.node.verbose', config) === true;
+  var config_js = process.argv.length > 0 ? config_js = process.argv.shift() :
+      Globals.resolve('test.node.config', config);
+  var tests = process.argv.length > 0 ? process.argv.slice(0) :
+      Globals.resolve('test.node.tests', config) || [];
+
+  if (!config_js || tests.length === 0)
+    fail_usage(1, 'Not enough arguments, must specify configuration and at least one test file.');
+
+  if (!fs.existsSync(config_js) || !fs.statSync(config_js).isFile())
+    fail(10, 'Could not find config file [' + config_js + ']');
 
   tests.forEach(function (file) {
     if (!fs.existsSync(file) || !fs.statSync(file).isFile())
@@ -80,17 +111,8 @@ module.exports = function (help_mode) {
   });
 
 
-  require('./../lib/kernel');
-  require('./../lib/loader');
-  require('./../lib/module');
-  require('./../lib/test');
+  var reporter = bolt.test.report.Logger.create(verbose);
+  var reader = bolt.kernel.fp.Func.curry(bolt.module.reader.Node.read, process.cwd() + '/.', config_js);
 
-  var Func = bolt.kernel.fp.Func;
-  var Node = bolt.module.reader.Node;
-  var Runner = bolt.test.run.Runner;
-
-  var reporter = bolt.test.report.Logger.create(verbose === 'true');
-  var reader = Func.curry(Node.read, process.cwd() + '/.', config);
-
-  Runner.run(reporter, reader, tests);
+  bolt.test.run.Runner.run(reporter, reader, tests);
 };
